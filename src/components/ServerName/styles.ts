15864e1323b8050d0a0d9c64946f9de3bac81ef4
igorsaaquino <igoralencar1985@gmail.com>
2022-10-10 17:04:33 -0300

    Resolvido problema com estilização

--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -8,6 +8,8 @@ export const Container = styled.div`
     padding: 0 11px 0 16px;
     
     background-color: var(--secondary);
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 1px;
+    z-index: 2;
 `;
 
 export const Title = styled.div`
