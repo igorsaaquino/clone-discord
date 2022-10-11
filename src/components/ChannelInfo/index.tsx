f328ef67216aef8b6656a9ce6dba5c427b2f8926
igorsaaquino <igoralencar1985@gmail.com>
2022-10-11 14:59:11 -0300

    Finalizado ChannelInfo CI

--- src/components/ChannelInfo/index.tsx
+++ src/components/ChannelInfo/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { Container, Title, ExpandIcon } from './styles';
+import { Container, HashtagIcon, Title, Separator, Description  } from './styles';
 
 const ChannelInfo: React.FC = () => {
     return (
     <Container>
-        <Title>Dev's Misticos</Title>
-        <ExpandIcon />
+        <HashtagIcon />
+        <Title>Geral</Title>
+        <Separator />
+        <Description>Canal aberto para conversa de texto</Description>
     </Container>
     );
 };
